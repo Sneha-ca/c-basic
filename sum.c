d86a1fdcb295a9f474723e08ce5227a21ef9f501
Sneha Kumar <snehakumarpchs@gmail.com>
2026-01-31 11:39:49 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+// add two num. taking input from user.
+
+# include<stdio.h>
+  int main(){
+  int a,b,c;
+  printf("Enter first number:");
+  scanf("%d",&a);
+  printf("Enetr second number:");
+  scanf("%d",&b);
+  c=(a+b);
+  printf("add=%i",c);
+  
+  return 0;
+}
